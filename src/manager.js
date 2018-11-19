38aae16ceb23146d28030bb96bdf231a40766e9e
Victor Truong <me@ifvictr.com>
2018-11-18 17:07:22 -0800

    Add user manager logic

--- src/manager.js
+++ src/manager.js
@@ -1,6 +1,31 @@
+const User = require('./user')
+
 class Manager {
     constructor(users = []) {
+        this.users = new Map()
 
+        // When only one player object is provided
+        if (!Array.isArray(users)) {
+            users = [users]
+        }
+        users.map(opts => this.addUser(opts))
+    }
+
+    getUser(username) {
+        return this.users.get(username)
+    }
+
+    addUser(opts) {
+        const user = new User(opts)
+        this.users.set(opts.username, user)
+    }
+
+    removeUser(username) {
+        return this.users.delete(username)
+    }
+
+    getUsers() {
+        return this.users
     }
 }
 
